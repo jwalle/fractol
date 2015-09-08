bc82457aa84855335430c592c48ad6ba08162bcf
Julien WALLE <jwalle@e2r8p21.42.fr>
2015-09-08 17:22:04 +0200

    init

--- srcs/mandelbrot.c
+++ srcs/mandelbrot.c
@@ -1,5 +1,28 @@
 #include "fractol.h"
 
+int		frac_mandel(int x, int y, t_env *e)
+{
+	int a;
+	int b;
+	int i;
+	int j;
+
+	a = x;
+	b = y;
+	i = 4;
+	j = 6;
+	(void)e;
+
+		while (i < 150)
+		{
+			x = a * a - b * b + i;
+			y = 2 * a + b + j;
+			if ((x * x + y * y) > 4)
+				return(i);
+		}
+		return (i);
+}
+
 void	draw_case_one(t_env *e)
 {
 	int			x;
@@ -9,15 +32,14 @@ void	draw_case_one(t_env *e)
 
 	// x = ori->x;
 	color = 0xFFFF00;
-	color2 = 0xFFFF00;
 	x = 0;
 	while (x <= WINDOW_HEIGHT)
 	{
 		y = 0;
 		while (y <= WINDOW_WIDE)
 		{
-			// color2 = ;
-			pxl_to_image(e, x, y, color * x);
+			color2 = frac_mandel(x, y, e);
+			pxl_to_image(e, x, y, color2);
 			y++;
 		}
 		x++;
